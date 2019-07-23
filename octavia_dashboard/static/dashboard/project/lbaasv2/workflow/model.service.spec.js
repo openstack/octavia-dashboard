0e190f92d74cbb2677a390a00b4826b6835b1e93
Gregory Thiemonge <gthiemon@redhat.com>
2019-07-23 17:35:52 +0200

    Added UDP support for listener and health-monitor
    - Added UDP protocol for listener and pool
    - Added UDP-CONNECT method for health-monitor
    
    Story: 1657091
    Task: 23225
    
    Change-Id: I3c25a1a69ef2e5ce0a994744a6a3a4fb5a3c3312

--- octavia_dashboard/static/dashboard/project/lbaasv2/workflow/model.service.spec.js
+++ octavia_dashboard/static/dashboard/project/lbaasv2/workflow/model.service.spec.js
@@ -465,7 +465,8 @@
       });
 
       it('has array of listener protocols', function() {
-        expect(model.listenerProtocols).toEqual(['HTTP', 'TCP', 'TERMINATED_HTTPS', 'HTTPS']);
+        expect(model.listenerProtocols).toEqual(['HTTP', 'TCP', 'TERMINATED_HTTPS', 'HTTPS',
+          'UDP']);
       });
 
       it('has array of pool lb_algorithms', function() {
@@ -477,7 +478,8 @@
       });
 
       it('has array of monitor types', function() {
-        expect(model.monitorTypes).toEqual(['HTTP', 'HTTPS', 'PING', 'TCP', 'TLS-HELLO']);
+        expect(model.monitorTypes).toEqual(['HTTP', 'HTTPS', 'PING', 'TCP', 'TLS-HELLO',
+          'UDP-CONNECT']);
       });
 
       it('has array of monitor http_methods', function() {
@@ -833,7 +835,7 @@
       });
 
       it('should initialize listener protocols', function() {
-        expect(model.listenerProtocols.length).toBe(4);
+        expect(model.listenerProtocols.length).toBe(5);
         expect(model.listenerProtocols.indexOf('TERMINATED_HTTPS')).toBe(2);
       });
     });
@@ -884,7 +886,7 @@
       });
 
       it('should initialize listener protocols', function() {
-        expect(model.listenerProtocols.length).toBe(3);
+        expect(model.listenerProtocols.length).toBe(4);
         expect(model.listenerProtocols.indexOf('TERMINATED_HTTPS')).toBe(-1);
       });
     });
